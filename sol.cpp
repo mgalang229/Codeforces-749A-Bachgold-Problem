227be62e1e3d36c079b36387de92a863dfa8a385
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-05-30 13:01:40 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, cnt = 0, t = 0;
+	cin >> n;
+	if(n%2 == 0){
+		while(t != n){
+			t += 2;
+			cnt++;
+		}
+		cout << cnt << "\n";
+		while(cnt--) cout << 2 << " " ;
+	} else{
+		while(t != n-1){
+			t += 2;
+			cnt++;
+		}
+		cout << cnt << "\n";
+		cnt--;
+		while(cnt--) cout << 2 << " ";
+		cout << 3;
+	}
+	cout << "\n";
+	return 0;
+}
